356e9387d9c68eeaf6c06409dd9db92a553f5fc3
Alex Ochs <mail@alexochs.de>
2023-03-19 17:19:52 +0100

    Update tg-webhook.ts

--- pages/api/tg-webhook.ts
+++ pages/api/tg-webhook.ts
@@ -1,9 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+type Update = {
+  update_id: number;
+  message: {
+    message_id: number;
+    from: any;
+    chat: any;
+    date: number;
+    text: string;
+  }
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req);
+  const update = req.body as Update;
+
+  console.log(JSON.stringify(update, null, 2));
+
   res.json({ success: true })
 }
