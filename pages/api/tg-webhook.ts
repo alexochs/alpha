3c068c6bbfb33fe72e25102ad18a28a4057af7b9
Alex Ochs <mail@alexochs.de>
2023-03-21 21:47:05 +0100

    Update tg-webhook.ts

--- pages/api/tg-webhook.ts
+++ pages/api/tg-webhook.ts
@@ -113,9 +113,9 @@ export default async function handler(
           };
       });
     
-    const filteredTasks = tasks.filter((task: any) => task.date === new Date().getTime());
+    const filteredTasks = tasks.filter((task: any) => task.date === new Date().setHours(0, 0, 0, 0));
 
-    console.log("Today: " + new Date().getTime());
+    console.log("Today: " + new Date().setHours(0, 0, 0, 0));
     //console.log("Tasks: " + JSON.stringify(tasks, null, 2));
     console.log("Filtered Tasks: " + JSON.stringify(filteredTasks, null, 2));
     
